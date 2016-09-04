5324abdf5c04bd784e62525d4f0e5c0c6978534a
vauxhall <zero.griffin@gmail.com>
2016-09-03 21:08:52 -0500

    controller home

--- expocity/Controller/Home/CHome.swift
+++ expocity/Controller/Home/CHome.swift
@@ -1,28 +1,18 @@
 import UIKit
 
-class CGame:CController
+class CHome:CController
 {
-    weak var viewGame:VGame!
-    weak var timer:NSTimer!
-    private let kTimerInterval:NSTimeInterval = 0.01
+    weak var viewHome:VHome!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
-        viewGame.load()
-        timer = NSTimer.scheduledTimerWithTimeInterval(kTimerInterval, target:self, selector:#selector(self.timerTick(sender:)), userInfo:nil, repeats:true)
     }
     
     override func loadView()
     {
-        let viewGame:VGame = VGame(controller:self)
-        self.viewGame = viewGame
-        view = viewGame
-    }
-    
-    func timerTick(sender timer:NSTimer)
-    {
-        viewGame.tick()
+        let viewHome:VHome = VHome(controller:self)
+        self.viewHome = viewHome
+        view = viewHome
     }
 }
