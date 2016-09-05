5b3e0434b0f3b17a8ab956e76082caf8e219d51c
vauxhall <zero.griffin@gmail.com>
2016-09-05 17:16:58 -0500

    view home create

--- expocity/Model/Home/MHomeItemCreate.swift
+++ expocity/Model/Home/MHomeItemCreate.swift
@@ -1,6 +1,13 @@
-import Foundation
+import UIKit
 
 class MHomeItemCreate:MHomeItem
 {
+    private let kCellHeight:CGFloat = 45
+    private let kSelectable:Bool = true
     
+    init()
+    {
+        let reusableIdentifier:String = VHomeCellCreate.reusableIdentifier()
+        super.init(cellHeight:kCellHeight, reusableIdentifier:reusableIdentifier, selectable:kSelectable)
+    }
 }
