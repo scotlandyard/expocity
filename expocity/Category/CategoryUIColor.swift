afe99f4a1fccbde7b0f769decdaa32e702e694a7
vauxhall <zero.griffin@gmail.com>
2016-09-04 20:48:36 -0500

    set main color

--- expocity/Category/CategoryUIColor.swift
+++ expocity/Category/CategoryUIColor.swift
@@ -4,7 +4,7 @@ extension UIColor
 {
     class func main() -> UIColor
     {
-        return UIColor(red:0.94901960784314, green:0.6078431372549, blue:0.18823529411765, alpha:1)
+        return UIColor(red:0.09803921568627, green:0.73333333333333, blue:0.94117647058824, alpha:1)
     }
     
     class func complement() -> UIColor
