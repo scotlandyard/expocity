55c2864213ca947cd7224ab9e29925305e2c2e15
vauxhall <zero.griffin@gmail.com>
2016-09-05 10:03:45 -0500

    updated main color

--- expocity/Category/CategoryUIColor.swift
+++ expocity/Category/CategoryUIColor.swift
@@ -4,7 +4,7 @@ extension UIColor
 {
     class func main() -> UIColor
     {
-        return UIColor(red:0.09803921568627, green:0.73333333333333, blue:0.94117647058824, alpha:1)
+        return UIColor(red:0.08235294117647, green:0.54509803921569, blue:0.76078431372549, alpha:1)
     }
     
     class func complement() -> UIColor
