4854442969c446bbdc00f1a0bca218aef5fe3d1e
vauxhall <zero.griffin@gmail.com>
2016-09-05 12:02:10 -0500

    cell size

--- expocity/View/Bar/VBar.swift
+++ expocity/View/Bar/VBar.swift
@@ -2,8 +2,9 @@ import UIKit
 
 class VBar:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
-    private let model:MMenu
     weak var parent:CParent!
+    private let model:MMenu
+    private let kCellWidth:CGFloat = 60
     
     init(parent:CParent)
     {
@@ -21,7 +22,46 @@ class VBar:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UIColle
         fatalError()
     }
     
+    //MARK: private
+    
+    private func modelAtIndex(index:NSIndexPath) -> MMenuItem
+    {
+        let item:MMenuItem = model.items[index.item]
+        
+        return item
+    }
+    
     //MARK: col del
     
+    func collectionView(collectionView:UICollectionView, layout collectionViewLayout:UICollectionViewLayout, sizeForItemAtIndexPath indexPath:NSIndexPath) -> CGSize
+    {
+        let height:CGFloat = collectionView.bounds.maxY
+        let size:CGSize = CGSizeMake(kCellWidth, height)
+        
+        return size
+    }
     
+    func numberOfSectionsInCollectionView(collectionView:UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        let count:Int = model.items.count
+        
+        return count
+    }
+    
+    func collectionView(collectionView:UICollectionView, cellForItemAtIndexPath indexPath:NSIndexPath) -> UICollectionViewCell
+    {
+        let item:MMenuItem = modelAtIndex(indexPath)
+        let cell:VBarCell = collectionView.dequeueReusableCellWithReuseIdentifier(
+            VBarCell.reusableIdentifier(),
+            forIndexPath:
+            indexPath) as! VBarCell
+        cell.config(item)
+        
+        return cell
+    }
 }
