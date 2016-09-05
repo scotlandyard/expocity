7313d5756d1bd4e52ee95724c87dc2bb8ce6151d
vauxhall <zero.griffin@gmail.com>
2016-09-05 17:25:55 -0500

    model home

--- expocity/Model/Home/MHome.swift
+++ expocity/Model/Home/MHome.swift
@@ -2,5 +2,16 @@ import Foundation
 
 class MHome
 {
+    let items:[MHomeItem]
     
+    init()
+    {
+        let itemTitle:MHomeItemTitle = MHomeItemTitle()
+        let itemCreate:MHomeItemCreate = MHomeItemCreate()
+        
+        items = [
+            itemTitle,
+            itemCreate
+        ]
+    }
 }
