6f064ce9f15e32f295abb8af486634017a639292
vauxhall <zero.griffin@gmail.com>
2016-09-05 17:16:28 -0500

    init cell title model

--- expocity/Model/Home/MHomeItemTitle.swift
+++ expocity/Model/Home/MHomeItemTitle.swift
@@ -1,6 +1,13 @@
-import Foundation
+import UIKit
 
 class MHomeItemTitle:MHomeItem
 {
+    private let kCellHeight:CGFloat = 65
+    private let kSelectable:Bool = false
     
+    init()
+    {
+        let reusableIdentifier:String = VHomeCellTitle.reusableIdentifier()
+        super.init(cellHeight:kCellHeight, reusableIdentifier:reusableIdentifier, selectable:kSelectable)
+    }
 }
