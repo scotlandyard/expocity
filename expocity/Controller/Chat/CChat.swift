85da52628636fed94c35c5cae0eaadceac327cd2
vauxhall <zero.griffin@gmail.com>
2016-09-05 17:05:43 -0500

    model chat

--- expocity/Controller/Chat/CChat.swift
+++ expocity/Controller/Chat/CChat.swift
@@ -2,5 +2,28 @@ import UIKit
 
 class CChat:CController
 {
+    weak var viewChat:VChat!
     
+    init(model)
+    {
+        super.init(nibName:nil, bundle:nil)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        title = NSLocalizedString("", comment:"")
+    }
+    
+    override func loadView()
+    {
+        let viewChat:VChat = VChat(controller:self)
+        self.viewChat = viewChat
+        view = viewChat
+    }
 }
---
+++ expocity/Model/Chat/MChat.swift
@@ -0,0 +1,9 @@
+//
+//  MChat.swift
+//  expocity
+//
+//  Created by zero on 9/5/16.
+//  Copyright © 2016 iturbide. All rights reserved.
+//
+
+import Foundation
