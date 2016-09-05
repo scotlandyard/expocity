332fce76832dcf634c2ccbe4e1bf24c3771307ed
vauxhall <zero.griffin@gmail.com>
2016-09-05 11:46:42 -0500

    menu item history

--- expocity/Model/Menu/MMenuItemHistory.swift
+++ expocity/Model/Menu/MMenuItemHistory.swift
@@ -1,9 +1,11 @@
-//
-//  MMenuItemHistory.swift
-//  expocity
-//
-//  Created by zero on 9/5/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class MMenuItemHistory:MMenuItem
+{
+    private let kIconImage:String = "menuSettings"
+    
+    init(index:Int)
+    {
+        super.init(iconImage:kIconImage, index:index)
+    }
+}
