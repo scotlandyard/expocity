2d80f416f7e3f40ec0e60b6d9721bc5c744ac615
vauxhall <zero.griffin@gmail.com>
2016-09-05 10:45:10 -0500

    added loader to home

--- expocity/View/Home/VHome.swift
+++ expocity/View/Home/VHome.swift
@@ -3,6 +3,7 @@ import UIKit
 class VHome:UIView
 {
     weak var controller:CHome!
+    weak var loader:VMainLoader!
     
     convenience init(controller:CHome)
     {
@@ -11,5 +12,26 @@ class VHome:UIView
         translatesAutoresizingMaskIntoConstraints = false
         userInteractionEnabled = false
         self.controller = controller
+        
+        let loader:VMainLoader = VMainLoader()
+        self.loader = loader
+        
+        addSubview(loader)
+        
+        let views:[String:AnyObject] = [
+            "loader":loader]
+        
+        let metrics:[String:AnyObject] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-0-[loader]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-0-[loader]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
 }
