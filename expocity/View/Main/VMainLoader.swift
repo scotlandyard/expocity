6f3249c137e63966a84caef03186c345c7eb32b0
vauxhall <zero.griffin@gmail.com>
2016-09-05 10:43:43 -0500

    view loader

--- expocity/View/Main/VMainLoader.swift
+++ expocity/View/Main/VMainLoader.swift
@@ -1,6 +1,33 @@
 import UIKit
 
-class VMainLoader:UIView
+class VMainLoader:UIImageView
 {
+    private let kAnimationDuration:NSTimeInterval = 1
     
+    init()
+    {
+        super.init(frame:CGRectZero)
+        
+        let images:[UIImage] = [
+            UIImage(named:"genericLoader0")!,
+            UIImage(named:"genericLoader1")!,
+            UIImage(named:"genericLoader2")!,
+            UIImage(named:"genericLoader3")!,
+            UIImage(named:"genericLoader4")!
+        ]
+        
+        userInteractionEnabled = false
+        translatesAutoresizingMaskIntoConstraints = false
+        clipsToBounds = true
+        animationDuration = kAnimationDuration
+        animationImages = images
+        contentMode = UIViewContentMode.Center
+        
+        startAnimating()
+    }
+    
+    required init?(coder aDecoder:NSCoder)
+    {
+        fatalError()
+    }
 }
