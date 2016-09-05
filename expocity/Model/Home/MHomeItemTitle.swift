fbefc2814df2998f491b8e79dad21a89b7948f45
vauxhall <zero.griffin@gmail.com>
2016-09-05 17:14:14 -0500

    view home cell

--- expocity/Model/Home/MHomeItemTitle.swift
+++ expocity/Model/Home/MHomeItemTitle.swift
@@ -1,9 +1,6 @@
-//
-//  MHomeItemTitle.swift
-//  expocity
-//
-//  Created by zero on 9/5/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class MHomeItemTitle:MHomeItem
+{
+    
+}
