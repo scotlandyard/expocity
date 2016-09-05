33bfb14ffdf4d5a4a280193616977c129b636b32
vauxhall <zero.griffin@gmail.com>
2016-09-05 10:05:43 -0500

    complement color

--- expocity/Category/CategoryUIColor.swift
+++ expocity/Category/CategoryUIColor.swift
@@ -2,14 +2,14 @@ import UIKit
 
 extension UIColor
 {
-    class func main() -> UIColor
+    class func main() -> UIColor //158BC2
     {
         return UIColor(red:0.08235294117647, green:0.54509803921569, blue:0.76078431372549, alpha:1)
     }
     
-    class func complement() -> UIColor
+    class func complement() -> UIColor //62CDF0
     {
-        return UIColor(red:0.94901960784314, green:0.32156862745098, blue:0.18039215686275, alpha:1)
+        return UIColor(red:0.3843137254902, green:0.80392156862745, blue:0.94117647058824, alpha:1)
     }
     
     class func collectionBackground() -> UIColor
