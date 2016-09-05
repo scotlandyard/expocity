60a6bd9c4d775c0b6499eb414c7b00fe16c46010
vauxhall <zero.griffin@gmail.com>
2016-09-05 09:40:43 -0500

    view bar cell

--- expocity/View/Bar/VBarCell.swift
+++ expocity/View/Bar/VBarCell.swift
@@ -1,9 +1,6 @@
-//
-//  VBarCell.swift
-//  expocity
-//
-//  Created by zero on 9/5/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class VBarCell:UICollectionViewCell
+{
+    
+}
