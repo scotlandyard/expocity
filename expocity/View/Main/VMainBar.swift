27152db0408dad7b5ed8a0a84e9b23ee528fb5bf
vauxhall <zero.griffin@gmail.com>
2016-09-04 20:43:10 -0500

    initializers for bar

--- expocity/View/Main/VMainBar.swift
+++ expocity/View/Main/VMainBar.swift
@@ -2,5 +2,16 @@ import UIKit
 
 class VMainBar:UIView
 {
+    weak var parent:CParent!
     
+    init(parent:CParent)
+    {
+        super.init(frame:CGRectZero)
+        self.parent = parent
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
 }
