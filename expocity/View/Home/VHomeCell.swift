2966614feb044fb155b5595a87875d503cf2b9c2
vauxhall <zero.griffin@gmail.com>
2016-09-05 17:49:55 -0500

    hover in home cell

--- expocity/View/Home/VHomeCell.swift
+++ expocity/View/Home/VHomeCell.swift
@@ -13,6 +13,36 @@ class VHomeCell:UICollectionViewCell
         fatalError()
     }
     
+    override var selected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var highlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        if selected || highlighted
+        {
+            alpha = 0.2
+        }
+        else
+        {
+            alpha = 1
+        }
+    }
+    
     //MARK: public
     
     func config(model:MHomeItem)
