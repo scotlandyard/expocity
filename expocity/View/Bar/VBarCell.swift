0c4ddb02b825ad8cd7b1c49e8052998a460e5d69
vauxhall <zero.griffin@gmail.com>
2016-09-05 11:35:03 -0500

    bar cell

--- expocity/View/Bar/VBarCell.swift
+++ expocity/View/Bar/VBarCell.swift
@@ -2,5 +2,13 @@ import UIKit
 
 class VBarCell:UICollectionViewCell
 {
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
 }
