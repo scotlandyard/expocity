32816ef207ff9d3279d3c4e33fca9f5f08831bef
vauxhall <zero.griffin@gmail.com>
2016-09-09 18:31:09 -0500

    sending margins

--- expocity/Model/Chat/MChatItemText.swift
+++ expocity/Model/Chat/MChatItemText.swift
@@ -4,11 +4,16 @@ class MChatItemText:MChatItem
 {
     let text:String
     let attributedString:NSAttributedString
+    let marginLeft:CGFloat
+    let marginRight:CGFloat
+    let kMaxHeight:CGFloat = 2000
     
-    init(reusableIdentifier:String, text:String, font:UIFont)
+    init(reusableIdentifier:String, text:String, font:UIFont, marginLeft:CGFloat, marginRight:CGFloat)
     {
         let attributes:[String:AnyObject] = [NSFontAttributeName:font]
         self.text = text
+        self.marginLeft = marginLeft
+        self.marginRight = marginRight
         attributedString = NSAttributedString(
             string:text,
             attributes:attributes)
--- expocity/Model/Chat/MChatItemTextMine.swift
+++ expocity/Model/Chat/MChatItemTextMine.swift
@@ -3,16 +3,21 @@ import UIKit
 class MChatItemTextMine:MChatItemText
 {
     private let kFontSize:CGFloat = 15
+    private let kMarginLeft:CGFloat = 50
+    private let kMarginRight:CGFloat = 10
     
     init(text:String)
     {
         let reusableIdentifier:String = VChatConversationCellTextMine.reusableIdentifier()
         let font:UIFont = UIFont.regular(kFontSize)
-        super.init(reusableIdentifier:reusableIdentifier, text:text, font:font)
+        super.init(reusableIdentifier:reusableIdentifier, text:text, font:font, marginLeft:kMarginLeft, marginRight:kMarginRight)
     }
     
     override func heightForCurrentWidth() -> CGFloat
     {
         let
+        let maxSize:CGSize = CGSizeMake(<#T##width: CGFloat##CGFloat#>, kMaxHeight)
+        let size:CGSize = attributedString.boundingRectWithSize(
+            <#T##size: CGSize##CGSize#>, options: <#T##NSStringDrawingOptions#>, context: <#T##NSStringDrawingContext?#>)
     }
 }
