1049eabb54d8cb57a2b70e33c5966461916df6fa
vauxhall <zero.griffin@gmail.com>
2016-09-10 16:53:52 -0500

    margin sums

--- expocity/Model/Chat/MChatItemTextMine.swift
+++ expocity/Model/Chat/MChatItemTextMine.swift
@@ -5,17 +5,20 @@ class MChatItemTextMine:MChatItemText
     private let kFontSize:CGFloat = 15
     private let kMarginLeft:CGFloat = 50
     private let kMarginRight:CGFloat = 10
+    private let kMarginTop:CGFloat = 10
+    private let kMarginBottom:CGFloat = 10
     
     init(text:String)
     {
         let reusableIdentifier:String = VChatConversationCellTextMine.reusableIdentifier()
         let font:UIFont = UIFont.regular(kFontSize)
-        super.init(reusableIdentifier:reusableIdentifier, text:text, font:font, marginLeft:kMarginLeft, marginRight:kMarginRight)
+        super.init(reusableIdentifier:reusableIdentifier, text:text, font:font, marginLeft:kMarginLeft, marginRight:kMarginRight, marginTop:kMarginTop, marginBottom:kMarginBottom)
     }
     
     override func heightForCurrentWidth() -> CGFloat
     {
-        let totalMarginHr:CGFloat
+        let totalMarginHr:CGFloat = marginLeft + marginRight
+        let totalMarginVr:CGFloat = marginTop + marginBottom
         let maxSize:CGSize = CGSizeMake(<#T##width: CGFloat##CGFloat#>, kMaxHeight)
         let size:CGSize = attributedString.boundingRectWithSize(
             <#T##size: CGSize##CGSize#>, options: <#T##NSStringDrawingOptions#>, context: <#T##NSStringDrawingContext?#>)
