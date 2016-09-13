6c5d84121e5eb7599195774440879be2c15711fa
vauxhall <zero.griffin@gmail.com>
2016-09-13 15:34:32 -0500

    collection in chat menu

--- expocity/View/Chat/VChatInputMenu.swift
+++ expocity/View/Chat/VChatInputMenu.swift
@@ -2,6 +2,53 @@ import UIKit
 
 class VChatInputMenu:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
+    weak var controller:CChat!
+    weak var collection:UICollectionView!
+    let model:MChatMenu
+    
+    init(controller:CChat)
+    {
+        model = MChatMenu()
+        
+        super.init(frame:CGRectZero)
+        clipsToBounds = true
+        backgroundColor = UIColor.clearColor()
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.headerReferenceSize = CGSizeZero
+        flow.footerReferenceSize = CGSizeZero
+        flow.minimumLineSpacing = 0
+        flow.minimumInteritemSpacing = 0
+        flow.scrollDirection = UICollectionViewScrollDirection.Horizontal
+        flow.sectionInset = UIEdgeInsetsZero
+        
+        let collection:UICollectionView = UICollectionView(frame:CGRectZero, collectionViewLayout:flow)
+        collection.clipsToBounds = true
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.showsHorizontalScrollIndicator = false
+        collection.showsVerticalScrollIndicator = false
+        collection.scrollEnabled = false
+        collection.bounces = false
+        collection.backgroundColor = UIColor.clearColor()
+        collection.dataSource = self
+        collection.delegate = self
+        collection.registerClass(
+            VChatInputMenuCell.self,
+            forCellWithReuseIdentifier:
+            VChatInputMenuCell.reusableIdentifier())
+        self.collection = collection
+        
+        addSubview(collection)
+        
+        
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+    
     //MARK: col del
     
     func numberOfSectionsInCollectionView(collectionView:UICollectionView) -> Int
