613fed628a408c46ded06ae5c7776d4743424778
vauxhall <zero.griffin@gmail.com>
2016-09-14 14:27:45 -0500

    constraints for close button

--- expocity/View/Chat/DisplayDetail/VChatDisplayDetailBar.swift
+++ expocity/View/Chat/DisplayDetail/VChatDisplayDetailBar.swift
@@ -3,6 +3,7 @@ import UIKit
 class VChatDisplayDetailBar:UIView
 {
     weak var controller:CChatDisplayDetail!
+    private let kCloseButtonWidth:CGFloat = 80
     
     convenience init(controller:CChatDisplayDetail)
     {
@@ -18,6 +19,23 @@ class VChatDisplayDetailBar:UIView
         closeButton.addTarget(self, action:#selector(self.actionClose(sender:)), forControlEvents:UIControlEvents.TouchUpInside)
         
         addSubview(closeButton)
+        
+        let views:[String:AnyObject] = [
+            "closeButton":closeButton]
+        
+        let metrics:[String:AnyObject] = [
+            "closeButtonWidth":kCloseButtonWidth]
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-0-[closeButton(closeButtonWidth)]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-0-[closeButton]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
     
     //MARK: actions
