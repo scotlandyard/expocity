598e07790736157af00269ee91872d01a4b44b83
vauxhall <zero.griffin@gmail.com>
2016-09-14 14:22:47 -0500

    adding bar in display chat

--- expocity/View/Chat/DisplayDetail/VChatDisplayDetail.swift
+++ expocity/View/Chat/DisplayDetail/VChatDisplayDetail.swift
@@ -3,6 +3,7 @@ import UIKit
 class VChatDisplayDetail:UIView
 {
     weak var controller:CChatDisplayDetail!
+    private let kBarHeight:CGFloat = 50
     
     convenience init(controller:CChatDisplayDetail)
     {
@@ -11,5 +12,26 @@ class VChatDisplayDetail:UIView
         backgroundColor = UIColor.clearColor()
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+        
+        let bar:VChatDisplayDetailBar = VChatDisplayDetailBar(controller:controller)
+        
+        addSubview(bar)
+        
+        let views:[String:AnyObject] = [
+            "bar":bar]
+        
+        let metrics:[String:AnyObject] = [
+            "barHeight":kBarHeight]
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-0-[bar]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-0-[bar(barHeight)]",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
 }
--- expocity/View/Chat/DisplayDetail/VChatDisplayDetailBar.swift
+++ expocity/View/Chat/DisplayDetail/VChatDisplayDetailBar.swift
@@ -2,5 +2,14 @@ import UIKit
 
 class VChatDisplayDetailBar:UIView
 {
+    weak var controller:CChatDisplayDetail!
     
+    convenience init(controller:CChatDisplayDetail)
+    {
+        self.init()
+        clipsToBounds = true
+        backgroundColor = UIColor.clearColor()
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
 }
