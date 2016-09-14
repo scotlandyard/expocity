16516cc051dea628e8eeecaa1bbb537104e508a2
vauxhall <zero.griffin@gmail.com>
2016-09-14 14:06:32 -0500

    init for view chat display detail

--- expocity/Controller/Chat/CChatDisplayDetail.swift
+++ expocity/Controller/Chat/CChatDisplayDetail.swift
@@ -2,5 +2,20 @@ import UIKit
 
 class CChatDisplayDetail:CController
 {
+    weak var viewDetail:VChatDisplayDetail!
     
+    init()
+    {
+        super.init(nibName:nil, bundle:nil)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+    
+    override func loadView()
+    {
+        let viewDetail:VChatDisplayDetail = VChatDisplayDetail(controller:self)
+    }
 }
--- expocity/View/Chat/DisplayDetail/VChatDisplayDetail.swift
+++ expocity/View/Chat/DisplayDetail/VChatDisplayDetail.swift
@@ -2,5 +2,11 @@ import UIKit
 
 class VChatDisplayDetail:UIView
 {
+    weak var controller:CChatDisplayDetail!
     
+    convenience init(controller:CChatDisplayDetail)
+    {
+        self.init()
+        self.controller = controller
+    }
 }
