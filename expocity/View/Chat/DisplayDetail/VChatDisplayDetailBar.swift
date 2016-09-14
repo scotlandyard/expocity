e959fd4e11dc2490c72b66a21a78656a9f3d1388
vauxhall <zero.griffin@gmail.com>
2016-09-14 14:26:28 -0500

    action close button

--- expocity/View/Chat/DisplayDetail/VChatDisplayDetailBar.swift
+++ expocity/View/Chat/DisplayDetail/VChatDisplayDetailBar.swift
@@ -11,5 +11,19 @@ class VChatDisplayDetailBar:UIView
         backgroundColor = UIColor.clearColor()
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+        
+        let closeButton:UIButton = UIButton()
+        closeButton.clipsToBounds = true
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.addTarget(self, action:#selector(self.actionClose(sender:)), forControlEvents:UIControlEvents.TouchUpInside)
+        
+        addSubview(closeButton)
+    }
+    
+    //MARK: actions
+    
+    func actionClose(sender button:UIButton)
+    {
+        controller.parent.pop()
     }
 }
