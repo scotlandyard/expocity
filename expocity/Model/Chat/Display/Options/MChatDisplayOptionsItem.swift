6c172410fd4b096545e8ddbe6f6194ed4a019ad7
vauxhall <zero.griffin@gmail.com>
2016-09-15 12:36:23 -0500

    init for model options item

--- expocity/Model/Chat/Display/Options/MChatDisplayOptionsItem.swift
+++ expocity/Model/Chat/Display/Options/MChatDisplayOptionsItem.swift
@@ -1,5 +1,15 @@
-import Foundation
+import UIKit
 
 class MChatDisplayOptionsItem
 {
+    let title:String
+    let icon:String
+    let contentMode:UIViewContentMode
+    
+    init(title:String, icon:String, contentMode:UIViewContentMode)
+    {
+        self.title = title
+        self.icon = icon
+        self.contentMode = contentMode
+    }
 }
---
+++ expocity/Model/Chat/Display/Options/MChatDisplayOptionsItemFit.swift
@@ -0,0 +1,9 @@
+//
+//  MChatDisplayOptionsItemFit.swift
+//  expocity
+//
+//  Created by zero on 9/15/16.
+//  Copyright © 2016 iturbide. All rights reserved.
+//
+
+import Foundation
