1186fc8eb16eed600e89918fe5cdbe3c741bba5a
vauxhall <zero.griffin@gmail.com>
2016-09-15 10:18:33 -0500

    init view chat display

--- expocity/Controller/Chat/CChatDisplayOptions.swift
+++ expocity/Controller/Chat/CChatDisplayOptions.swift
@@ -2,5 +2,30 @@ import UIKit
 
 class CChatDisplayOptions:CController
 {
+    weak var viewOptions:VChatDisplayOptions!
     
+    init()
+    {
+        super.init(nibName:nil, bundle:nil)
+        modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
+        modalTransitionStyle = UIModalTransitionStyle.CrossDissolve
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+    
+    override func loadView()
+    {
+        let viewOptions:VChatDisplayOptions = VChatDisplayOptions(controller:self)
+        self.viewOptions = viewOptions
+        view = viewOptions
+    }
+    
+    override func viewDidAppear(animated:Bool)
+    {
+        super.viewDidAppear(animated)
+        viewDetail.animateImage()
+    }
 }
--- expocity/View/Chat/DisplayOptions/VChatDisplayOptions.swift
+++ expocity/View/Chat/DisplayOptions/VChatDisplayOptions.swift
@@ -2,5 +2,14 @@ import UIKit
 
 class VChatDisplayOptions:UIView
 {
+    weak var controller:CChatDisplayOptions!
     
+    convenience init(controller:CChatDisplayOptions)
+    {
+        self.init()
+        clipsToBounds = true
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        self.controller = controller
+    }
 }
