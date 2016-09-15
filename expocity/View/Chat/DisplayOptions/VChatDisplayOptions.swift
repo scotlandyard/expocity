ee8579da117f420cc1a6bf359bb270190282a8fd
vauxhall <zero.griffin@gmail.com>
2016-09-15 10:21:30 -0500

    blur in view options

--- expocity/View/Chat/DisplayOptions/VChatDisplayOptions.swift
+++ expocity/View/Chat/DisplayOptions/VChatDisplayOptions.swift
@@ -3,14 +3,29 @@ import UIKit
 class VChatDisplayOptions:UIView
 {
     weak var controller:CChatDisplayOptions!
+    weak var blur:UIView!
+    private let kAnimationDuration:NSTimeInterval = 0.3
     
     convenience init(controller:CChatDisplayOptions)
     {
         self.init()
         clipsToBounds = true
         translatesAutoresizingMaskIntoConstraints = false
-        
+        backgroundColor = UIColor.clearColor()
         self.controller = controller
+        
+        let blurEffect:UIBlurEffect = UIBlurEffect(style:UIBlurEffectStyle.Light)
+        let visualEffect:UIVisualEffectView = UIVisualEffectView(effect:blurEffect)
+        visualEffect.translatesAutoresizingMaskIntoConstraints = false
+        visualEffect.userInteractionEnabled = false
+        
+        let blur:UIView = UIView()
+        blur.translatesAutoresizingMaskIntoConstraints = false
+        blur.userInteractionEnabled = false
+        blur.alpha = 0
+        self.blur = blur
+        
+        blur.addSubview(visualEffect)
     }
     
     //MARK: public
