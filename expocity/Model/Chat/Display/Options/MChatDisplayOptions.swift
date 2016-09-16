690c9e1179b230b625aa9b956da567185d745391
vauxhall <zero.griffin@gmail.com>
2016-09-16 16:58:59 -0500

    options struct

--- expocity/Model/Chat/Display/Options/MChatDisplayOptions.swift
+++ expocity/Model/Chat/Display/Options/MChatDisplayOptions.swift
@@ -2,5 +2,18 @@ import Foundation
 
 class MChatDisplayOptions
 {
+    let items:[MChatDisplayOptionsItem]
     
+    init()
+    {
+        let itemFit:MChatDisplayOptionsItemFit = MChatDisplayOptionsItemFit()
+        let itemFill:MChatDisplayOptionsItemFill = MChatDisplayOptionsItemFill()
+        let itemCenter:MChatDisplayOptionsItemCenter = MChatDisplayOptionsItemCenter()
+        
+        items = [
+            itemFit,
+            itemFill,
+            itemCenter
+        ]
+    }
 }
