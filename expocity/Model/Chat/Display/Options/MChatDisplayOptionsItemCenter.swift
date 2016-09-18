a3900366800556304b5595f11ed83b5f4bf7f21a
vauxhall <zero.griffin@gmail.com>
2016-09-18 12:48:35 -0500

    wording center item

--- expocity/Model/Chat/Display/Options/MChatDisplayOptionsItemCenter.swift
+++ expocity/Model/Chat/Display/Options/MChatDisplayOptionsItemCenter.swift
@@ -7,7 +7,7 @@ class MChatDisplayOptionsItemCenter:MChatDisplayOptionsItem
     
     init()
     {
-        let title:String = NSLocalizedString("", comment:"")
+        let title:String = NSLocalizedString("MChatDisplayOptionsItemCenter_title", comment:"")
         
         super.init(title:title, icon:kIcon, contentMode:kContentMode)
     }
