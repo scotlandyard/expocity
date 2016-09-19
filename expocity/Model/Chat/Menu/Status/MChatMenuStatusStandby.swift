5106b6365247fa419b64a3cbed6f689ab4d51eaa
vauxhall <zero.griffin@gmail.com>
2016-09-19 13:42:07 -0500

    item remove picture

--- expocity/Model/Chat/Menu/Status/MChatMenuStatusStandby.swift
+++ expocity/Model/Chat/Menu/Status/MChatMenuStatusStandby.swift
@@ -5,11 +5,13 @@ class MChatMenuStatusStandby:MChatMenuStatus
     init()
     {
         let itemPicture:MChatMenuItemPicture = MChatMenuItemPicture()
+        let itemRemovePicture:MChatMenuItemRemovePicture = MChatMenuItemRemovePicture()
         let itemOptions:MChatMenuItemOptions = MChatMenuItemOptions()
         let itemAnnotate:MChatMenuItemAnnotate = MChatMenuItemAnnotate()
         
         let items:[MChatMenuItem] = [
             itemPicture,
+            itemRemovePicture,
             itemOptions,
             itemAnnotate
         ]
