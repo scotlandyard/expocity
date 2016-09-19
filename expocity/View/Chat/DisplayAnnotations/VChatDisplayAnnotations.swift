e942306ba3df57c5c37ceb576784bba1313f8a76
vauxhall <zero.griffin@gmail.com>
2016-09-19 17:26:44 -0500

    shade top and bottom

--- expocity/View/Chat/DisplayAnnotations/VChatDisplayAnnotations.swift
+++ expocity/View/Chat/DisplayAnnotations/VChatDisplayAnnotations.swift
@@ -4,7 +4,7 @@ class VChatDisplayAnnotations:UIView
 {
     weak var controller:CChatDisplayAnnotations!
     weak var shadeTop:UIView!
-    weak var shadeBotton:UIView!
+    weak var shadeBottom:UIView!
     private let kAnimateDuration:NSTimeInterval = 0.3
     
     convenience init(controller:CChatDisplayAnnotations)
@@ -15,7 +15,26 @@ class VChatDisplayAnnotations:UIView
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
         
+        let shadeColor:UIColor = UIColor(white:0, alpha:0.6)
         
+        let shadeTop:UIView = UIView()
+        shadeTop.userInteractionEnabled = false
+        shadeTop.translatesAutoresizingMaskIntoConstraints = false
+        shadeTop.clipsToBounds = true
+        shadeTop.backgroundColor = shadeColor
+        shadeTop.alpha = 0
+        self.shadeTop = shadeTop
+        
+        let shadeBottom:UIView = UIView()
+        shadeBottom.userInteractionEnabled = false
+        shadeBottom.translatesAutoresizingMaskIntoConstraints = false
+        shadeBottom.clipsToBounds = true
+        shadeBottom.backgroundColor = shadeColor
+        shadeBottom.alpha = 0
+        self.shadeBottom = shadeBottom
+        
+        addSubview(shadeTop)
+        addSubview(shadeBottom)
         
         let screenRect:CGRect = UIScreen.mainScreen().bounds
         let topMargin:CGFloat = controller.imageRect.minY
