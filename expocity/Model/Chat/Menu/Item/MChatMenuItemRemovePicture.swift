22abfdad0f31e7feec00a50a1a838974a8db3818
vauxhall <zero.griffin@gmail.com>
2016-09-19 13:50:45 -0500

    removing image

--- expocity/Model/Chat/Menu/Item/MChatMenuItemRemovePicture.swift
+++ expocity/Model/Chat/Menu/Item/MChatMenuItemRemovePicture.swift
@@ -12,6 +12,6 @@ class MChatMenuItemRemovePicture:MChatMenuItem
     override func selected(controller:CChat)
     {
         UIApplication.sharedApplication().keyWindow?.endEditing(true)
-        controller.viewChat.presentImagePicker()
+        controller.viewChat.display.removeImage()
     }
 }
