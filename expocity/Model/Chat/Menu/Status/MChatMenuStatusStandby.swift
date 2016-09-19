b283a8784f6ebb894f3619d385070cc49bd6fd55
vauxhall <zero.griffin@gmail.com>
2016-09-19 14:18:24 -0500

    truncate status standby

--- expocity/Model/Chat/Menu/Status/MChatMenuStatusStandby.swift
+++ expocity/Model/Chat/Menu/Status/MChatMenuStatusStandby.swift
@@ -5,16 +5,10 @@ class MChatMenuStatusStandby:MChatMenuStatus
     init()
     {
         let itemPicture:MChatMenuItemPicture = MChatMenuItemPicture()
-        let itemRemovePicture:MChatMenuItemRemovePicture = MChatMenuItemRemovePicture()
-        let itemOptions:MChatMenuItemOptions = MChatMenuItemOptions()
-        let itemAnnotate:MChatMenuItemAnnotate = MChatMenuItemAnnotate()
         let itemEmoji:MChatMenuItemEmoji = MChatMenuItemEmoji()
         
         let items:[MChatMenuItem] = [
             itemPicture,
-            itemRemovePicture,
-            itemOptions,
-            itemAnnotate,
             itemEmoji
         ]
         
