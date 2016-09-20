dfbfe6226c0476c6699dbf5928083231a4851e6a
vauxhall <zero.griffin@gmail.com>
2016-09-19 19:59:41 -0500

    backButton in annotation bar

--- expocity/View/Chat/DisplayAnnotations/VChatDisplayAnnotationsBar.swift
+++ expocity/View/Chat/DisplayAnnotations/VChatDisplayAnnotationsBar.swift
@@ -2,5 +2,65 @@ import UIKit
 
 class VChatDisplayAnnotationBar:UIView
 {
+    weak var controller:CChatDisplayAnnotations!
     
+    convenience init(controller:CChatDisplayAnnotations)
+    {
+        self.init()
+        clipsToBounds = true
+        backgroundColor = UIColor.clearColor()
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+        
+        let border:UIView = UIView()
+        border.userInteractionEnabled = false
+        border.translatesAutoresizingMaskIntoConstraints = false
+        border.backgroundColor = UIColor(white:1, alpha:0.5)
+        
+        let backButton:UIButton = UIButton()
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.setImage(UIImage(named:"genericBack"), forState:UIControlState.Normal)
+        backButton.imageView!.clipsToBounds = true
+        backButton.imageView!.contentMode = UIViewContentMode.Center
+        backButton.addTarget(self, action:#selector(self.actionBack(sender:)), forControlEvents:UIControlEvents.TouchUpInside)
+        backButton.alpha = 0
+        backButton.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 25)
+        
+        addSubview(border)
+        addSubview(backButton)
+        
+        let views:[String:AnyObject] = [
+            "border":border,
+            "backButton":backButton]
+        
+        let metrics:[String:AnyObject] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-0-[border]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-0-[backButton(60)]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:[border(1)]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:[backButton(44)]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+    }
+    
+    //MARK: actions
+    
+    func actionBack(sender button:UIButton)
+    {
+        controller.parent.dismiss()
+    }
 }
