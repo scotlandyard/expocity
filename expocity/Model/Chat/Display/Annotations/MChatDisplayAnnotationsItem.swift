6ad394759feb2892fe3f28b58a9e61b2ce48716a
vauxhall <zero.griffin@gmail.com>
2016-09-19 20:37:25 -0500

    properties of annotations item

--- expocity/Model/Chat/Display/Annotations/MChatDisplayAnnotationsItem.swift
+++ expocity/Model/Chat/Display/Annotations/MChatDisplayAnnotationsItem.swift
@@ -1,6 +1,15 @@
-import Foundation
+import UIKit
 
 class MChatDisplayAnnotationsItem
 {
+    let text:String
+    let xPos:CGFloat
+    let yPos:CGFloat
     
+    init(text:String, xPos:CGFloat, yPos:CGFloat)
+    {
+        self.text = text
+        self.xPos = xPos
+        self.yPos = yPos
+    }
 }
