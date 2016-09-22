307544470370fac867d784f88a467e812bed598d
vauxhall <zero.griffin@gmail.com>
2016-09-22 15:49:05 -0500

    annotation list cell init

--- expocity/View/Chat/DisplayAnnotations/VChatDisplayAnnotationsListCell.swift
+++ expocity/View/Chat/DisplayAnnotations/VChatDisplayAnnotationsListCell.swift
@@ -2,6 +2,17 @@ import UIKit
 
 class VChatDisplayAnnotationsListCell:UICollectionViewCell
 {
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clearColor()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
     
     //MARK: public
     
