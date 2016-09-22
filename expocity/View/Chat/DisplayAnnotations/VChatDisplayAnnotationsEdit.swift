35d841b76c668676297b38fe669cbc24cd1e09f1
vauxhall <zero.griffin@gmail.com>
2016-09-22 17:56:17 -0500

    editText view

--- expocity/View/Chat/DisplayAnnotations/VChatDisplayAnnotationsEdit.swift
+++ expocity/View/Chat/DisplayAnnotations/VChatDisplayAnnotationsEdit.swift
@@ -89,42 +89,6 @@ class VChatDisplayAnnoationsEdit:UIView, UITextFieldDelegate
     
     //MARK: actions
     
-    func actionRemove(sender button:UIButton)
-    {
-        /*
-        UIApplication.sharedApplication().keyWindow!.endEditing(true)
-        
-        let alert:UIAlertController = UIAlertController(
-            title:NSLocalizedString("CNotifications_title", comment:""),
-            message:
-            item.title,
-            preferredStyle:UIAlertControllerStyle.ActionSheet)
-        
-        let actionDo:UIAlertAction = UIAlertAction(
-            title:NSLocalizedString("CNotifications_actionDo", comment:""),
-            style:
-            UIAlertActionStyle.Default)
-        { [weak self] (action) in
-            
-            if self != nil
-            {
-                item.executeAction(self!)
-            }
-            
-            let titleDone:String = NSLocalizedString("CNotifications_actionDone", comment:"")
-            VMainAlert.Message(titleDone)
-        }
-        
-        let actionCancel:UIAlertAction = UIAlertAction(
-            title:NSLocalizedString("CNotifications_actionCancel", comment:""),
-            style:UIAlertActionStyle.Cancel,
-            handler:nil)
-        
-        alert.addAction(actionDo)
-        alert.addAction(actionCancel)
-        presentViewController(alert, animated:true, completion:nil)*/
-    }
-    
     func actionDone(sender button:UIButton)
     {
         confirmText()
