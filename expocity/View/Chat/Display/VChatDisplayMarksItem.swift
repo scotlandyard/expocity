e290887d316b66ed60e39eb2c09c15ac86f9db35
vauxhall <zero.griffin@gmail.com>
2016-09-23 12:48:01 -0500

    imageview in mark item

--- expocity/View/Chat/Display/VChatDisplayMarksItem.swift
+++ expocity/View/Chat/Display/VChatDisplayMarksItem.swift
@@ -1,6 +1,19 @@
 import UIKit
 
-class VChatDisplayMarksItem:UIView
+class VChatDisplayMarksItem:UIButton
 {
+    weak var controller:CChat!
     
+    convenience init(controller:CChat)
+    {
+        self.init()
+        clipsToBounds = true
+        translatesAutoresizingMaskIntoConstraints = false
+        setImage(UIImage(named:"chatAnnotation"), forState:UIControlState.Normal)
+        setImage(UIImage(named:"chatAnnotationSelected"), forState:UIControlState.Highlighted)
+        setImage(UIImage(named:"chatAnnotationSelected"), forState:UIControlState.Selected)
+        imageView!.contentMode = UIViewContentMode.Center
+        imageView!.clipsToBounds = true
+        self.controller = controller
+    }
 }
