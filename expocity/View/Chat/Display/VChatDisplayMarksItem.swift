a5701fbf27dab66e1566ed61e117622d607f73a7
vauxhall <zero.griffin@gmail.com>
2016-09-23 14:29:25 -0500

    item focused

--- expocity/View/Chat/Display/VChatDisplayMarksItem.swift
+++ expocity/View/Chat/Display/VChatDisplayMarksItem.swift
@@ -12,8 +12,21 @@ class VChatDisplayMarksItem:UIButton
         setImage(UIImage(named:"chatAnnotation"), forState:UIControlState.Normal)
         setImage(UIImage(named:"chatAnnotationSelected"), forState:UIControlState.Highlighted)
         setImage(UIImage(named:"chatAnnotationSelected"), forState:UIControlState.Selected)
+        setImage(UIImage(named:"chatAnnotationSelected"), forState:UIControlState.Focused)
         imageView!.contentMode = UIViewContentMode.Center
         imageView!.clipsToBounds = true
+        addTarget(self, action:#selector(self.actionButton(sender:)), forControlEvents:UIControlEvents.TouchUpInside)
         self.controller = controller
+    }
+    
+    //MARK: actions
+    
+    func actionButton(sender button:UIButton)
+    {
+        if !selected
+        {
+            selected = true
+            print("signos")
+        }
     }
 }
