2cb2ce859b66581579cd190dcb3a3b2e7f59c7be
vauxhall <zero.griffin@gmail.com>
2016-09-23 13:25:40 -0500

    constraints mark item

--- expocity/View/Chat/Display/VChatDisplayMarks.swift
+++ expocity/View/Chat/Display/VChatDisplayMarks.swift
@@ -3,10 +3,15 @@ import UIKit
 class VChatDisplayMarks:UIView
 {
     weak var controller:CChat!
+    weak var itemsBase:UIView?
+    private let kItemSize:CGFloat = 50
+    private let itemSize_2:CGFloat
     
-    convenience init(controller:CChat)
+    init(controller:CChat)
     {
-        self.init()
+        itemSize_2 = kItemSize / 2.0
+        
+        super.init(frame:CGRectZero)
         clipsToBounds = false
         backgroundColor = UIColor(red:1, green:0, blue:0, alpha:0.2)
         translatesAutoresizingMaskIntoConstraints = false
@@ -35,10 +40,85 @@ class VChatDisplayMarks:UIView
             views:views))
     }
     
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+    
     //MARK: actions
     
     func actionButton(sender button:UIButton)
     {
         controller.displayDetail()
+    }
+    
+    //MARK: public
+    
+    func addItems()
+    {
+        let screenSize:CGSize = UIScreen.mainScreen().bounds.size
+        let imageWidth:CGFloat
+        let imageHeight:CGFloat = controller.viewChat.display.maxHeight
+        
+        if screenSize.width < screenSize.height
+        {
+            imageWidth = screenSize.width
+        }
+        else
+        {
+            imageWidth = screenSize.height
+        }
+        
+        self.itemsBase?.removeFromSuperview()
+        
+        let itemsBase:UIView = UIView()
+        self.itemsBase = itemsBase
+        addSubview(itemsBase)
+        
+        let views:[String:AnyObject] = [
+            "itemsBase":itemsBase]
+        
+        let metrics:[String:AnyObject] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-0-[itemsBase]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-0-[itemsBase]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        
+        for modelItem:MChatDisplayAnnotationsItem in controller.model.annotations.items
+        {
+            let viewItem:VChatDisplayMarksItem = VChatDisplayMarksItem(controller:controller)
+            itemsBase.addSubview(viewItem)
+            
+            let percentX:CGFloat = modelItem.xPercent * imageWidth
+            let percentY:CGFloat = modelItem.yPercent * imageHeight
+            let itemLeft:CGFloat = percentX - itemSize_2
+            let itemTop:CGFloat = percentY - itemSize_2
+            
+            let itemViews:[String:AnyObject] = [
+                "viewItem":viewItem]
+            
+            let itemMetrics:[String:AnyObject] = [
+                "itemSize":kItemSize,
+                "itemLeft":itemLeft,
+                "itemTop":itemTop]
+            
+            addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+                "H:|-(itemLeft)-[viewItem(itemSize)]",
+                options:[],
+                metrics:itemMetrics,
+                views:itemViews))
+            addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+                "V:|-(itemTop)-[viewItem(itemSize)]",
+                options:[],
+                metrics:itemMetrics,
+                views:itemViews))
+        }
     }
 }
