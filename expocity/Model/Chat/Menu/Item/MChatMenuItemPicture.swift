14e02c6f8de5ad47d657dd8a1afe1e3975f88306
vauxhall <zero.griffin@gmail.com>
2016-09-23 17:19:52 -0500

    fun show alert

--- expocity/Model/Chat/Menu/Item/MChatMenuItemPicture.swift
+++ expocity/Model/Chat/Menu/Item/MChatMenuItemPicture.swift
@@ -14,4 +14,42 @@ class MChatMenuItemPicture:MChatMenuItem
         UIApplication.sharedApplication().keyWindow!.endEditing(true)
         controller.viewChat.presentImagePicker()
     }
+    
+    //MARK: private
+    
+    private func showAlert()
+    {
+        UIApplication.sharedApplication().keyWindow!.endEditing(true)
+        let text:String = model.text
+        
+        let alert:UIAlertController = UIAlertController(
+            title:NSLocalizedString("VChatDisplayAnnotationsListCell_removeTitle", comment:""),
+            message:text,
+            preferredStyle:UIAlertControllerStyle.ActionSheet)
+        
+        let actionDo:UIAlertAction = UIAlertAction(
+            title:NSLocalizedString("VChatDisplayAnnotationsListCell_removeButton", comment:""),
+            style:
+            UIAlertActionStyle.Default)
+        { [weak self] (action) in
+            
+            if self != nil
+            {
+                self!.controller.removeAnnotation(self!.model)
+            }
+        }
+        
+        let actionCancel:UIAlertAction = UIAlertAction(
+            title:NSLocalizedString("VChatDisplayAnnotationsListCell_removeCancel", comment:""),
+            style:UIAlertActionStyle.Cancel,
+            handler:nil)
+        
+        alert.addAction(actionDo)
+        alert.addAction(actionCancel)
+        
+        controller.parent.presentViewController(
+            alert,
+            animated:true,
+            completion:nil)
+    }
 }
