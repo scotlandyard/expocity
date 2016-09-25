69138aa1aa8773693bb8bd788074373b9f82361c
vauxhall <zero.griffin@gmail.com>
2016-09-24 19:21:08 -0500

    collection delegate

--- expocity/View/Chat/Emoji/Keyboard/VChatEmojiKeyboard.swift
+++ expocity/View/Chat/Emoji/Keyboard/VChatEmojiKeyboard.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class VChatEmojiKeyboard:UIView
+class VChatEmojiKeyboard:UIView, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout
 {
     weak var controller:CChat!
     weak var collectionView:UICollectionView!
@@ -22,5 +22,22 @@ class VChatEmojiKeyboard:UIView
     required init?(coder:NSCoder)
     {
         fatalError()
+    }
+    
+    //MARK: collection del
+    
+    func numberOfSectionsInCollectionView(collectionView:UICollectionView) -> Int
+    {
+        return 0
+    }
+    
+    func collectionView(collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        return 0
+    }
+    
+    func collectionView(collectionView:UICollectionView, cellForItemAtIndexPath indexPath:NSIndexPath) -> UICollectionViewCell
+    {
+        return nil
     }
 }
