dd9a003683cf9afffc7946e073fd46f3ed439b77
vauxhall <zero.griffin@gmail.com>
2016-09-24 19:14:33 -0500

    keyboard view init

--- expocity/View/Chat/Emoji/Keyboard/VChatEmojiKeyboard.swift
+++ expocity/View/Chat/Emoji/Keyboard/VChatEmojiKeyboard.swift
@@ -2,5 +2,15 @@ import UIKit
 
 class VChatEmojiKeyboard:UIView
 {
+    weak var controller:CChat!
     
+    init(controller:CChat)
+    {
+        super.init(frame:CGRectZero)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
 }
