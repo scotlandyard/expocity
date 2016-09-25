c518ea69d8eea75b4e0bb53fc24d45f9f9d41e94
vauxhall <zero.griffin@gmail.com>
2016-09-24 19:15:35 -0500

    collectionView

--- expocity/View/Chat/Emoji/Keyboard/VChatEmojiKeyboard.swift
+++ expocity/View/Chat/Emoji/Keyboard/VChatEmojiKeyboard.swift
@@ -3,10 +3,20 @@ import UIKit
 class VChatEmojiKeyboard:UIView
 {
     weak var controller:CChat!
+    weak var collectionView:UICollectionView!
     
     init(controller:CChat)
     {
         super.init(frame:CGRectZero)
+        clipsToBounds = true
+        backgroundColor = UIColor.whiteColor()
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        
+        let collectionView:UICollectionView = UICollectionView(frame:CGRectZero, collectionViewLayout:flow)
+        
+        addSubview(collectionView)
     }
     
     required init?(coder:NSCoder)
