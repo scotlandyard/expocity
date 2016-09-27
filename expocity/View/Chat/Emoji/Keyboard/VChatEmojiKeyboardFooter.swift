b6ebc4fd3cc0ad8ea48e01335c7a65d1a4310e5e
vauxhall <zero.griffin@gmail.com>
2016-09-27 14:04:45 -0500

    hide emoji keyboard

--- expocity/View/Chat/Emoji/Keyboard/VChatEmojiKeyboardFooter.swift
+++ expocity/View/Chat/Emoji/Keyboard/VChatEmojiKeyboardFooter.swift
@@ -10,11 +10,42 @@ class VChatEmojiKeyboardFooter:UICollectionReusableView
         clipsToBounds = true
         backgroundColor = UIColor.clear
         
+        let button:UIButton = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(#imageLiteral(resourceName: "chatHideKeyboard"), for:UIControlState.normal)
+        button.addTarget(
+            self,
+            action:#selector(self.actionClose(sender:)),
+            for:UIControlEvents.touchUpInside)
         
+        addSubview(button)
+        
+        let views:[String:UIView] = [
+            "button":button]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[button]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[button]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
     
     required init?(coder:NSCoder)
     {
         fatalError()
+    }
+    
+    //MARK: actions
+    
+    func actionClose(sender button:UIButton)
+    {
+        controller.viewChat.
     }
 }
