6743535875dca26141d81d318ba15b9ed8bbd255
vauxhall <zero.griffin@gmail.com>
2016-09-27 16:42:03 -0500

    init model emoji mine

--- expocity/Model/Chat/Item/Emoji/MChatItemEmojiMine.swift
+++ expocity/Model/Chat/Item/Emoji/MChatItemEmojiMine.swift
@@ -4,7 +4,9 @@ class MChatItemEmojiMine:MChatItemEmoji
 {
     init(image:UIImage)
     {
+        let reusableIdentifier:String = VChatConversationCellEmojiMine.reusableIdentifier()
         
+        super.init(image:image, reusableIdentifier:reusableIdentifier)
     }
     
     override func heightForCurrentWidth() -> CGFloat
