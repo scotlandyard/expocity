5d12d74167712fa73945894acb00fbd31481ade6
vauxhall <zero.griffin@gmail.com>
2016-09-27 16:03:27 -0500

    model chat emoji mine

--- expocity/Model/Chat/Item/Emoji/MChatItemEmoji.swift
+++ expocity/Model/Chat/Item/Emoji/MChatItemEmoji.swift
@@ -2,5 +2,21 @@ import UIKit
 
 class MChatItemEmoji:MChatItem
 {
+    let image:UIImage
+    private let kHeight:CGFloat = 50
     
+    init(image:UIImage)
+    {
+        self.image = image
+        let reusableIdentifier:String = VChatConversationCellTextMine.reusableIdentifier()
+        
+        super.init(reusableIdentifier:reusableIdentifier)
+    }
+    
+    override func heightForCurrentWidth() -> CGFloat
+    {
+        let textHeight:CGFloat = textContentHeight()
+        
+        return textHeight
+    }
 }
