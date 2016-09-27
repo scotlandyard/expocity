b92f8596447355298d7c36193333b31c75a0bfcb
vauxhall <zero.griffin@gmail.com>
2016-09-27 14:14:14 -0500

    keyboard items

--- expocity/Model/Chat/Emoji/Keyboard/MChatEmojiKeyboardItemAngry.swift
+++ expocity/Model/Chat/Emoji/Keyboard/MChatEmojiKeyboardItemAngry.swift
@@ -1,9 +1,6 @@
-//
-//  MChatEmojiKeyboardItemAngry.swift
-//  expocity
-//
-//  Created by zero on 9/27/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class MChatEmojiKeyboardItemAngry:MChatEmojiKeyboardItem
+{
+    
+}
--- expocity/Model/Chat/Emoji/Keyboard/MChatEmojiKeyboardItemSad.swift
+++ expocity/Model/Chat/Emoji/Keyboard/MChatEmojiKeyboardItemSad.swift
@@ -1,9 +1,6 @@
-//
-//  MChatEmojiKeyboardItemSad.swift
-//  expocity
-//
-//  Created by zero on 9/27/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class MChatEmojiKeyboardItemSad:MChatEmojiKeyboardItem
+{
+    
+}
