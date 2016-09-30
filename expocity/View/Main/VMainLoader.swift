d1b1af2b3981ab63513c21db8ba34dbb9195a578
vauxhall <zero.griffin@gmail.com>
2016-09-30 15:28:23 -0500

    generic loaders using literals

--- expocity/View/Main/VMainLoader.swift
+++ expocity/View/Main/VMainLoader.swift
@@ -9,10 +9,10 @@ class VMainLoader:UIImageView
         super.init(frame:CGRect.zero)
         
         let images:[UIImage] = [
-            UIImage(named:"genericLoader0")!,
-            UIImage(named:"genericLoader1")!,
-            UIImage(named:"genericLoader2")!,
-            UIImage(named:"genericLoader3")!
+            #imageLiteral(resourceName: "genericLoader0"),
+            #imageLiteral(resourceName: "genericLoader1"),
+            #imageLiteral(resourceName: "genericLoader2"),
+            #imageLiteral(resourceName: "genericLoader3")!
         ]
         
         isUserInteractionEnabled = false
