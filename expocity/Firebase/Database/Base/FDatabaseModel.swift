7acc5e9ccc1e273b98ae6380bee2e04ea93a18d6
vauxhall <zero.griffin@gmail.com>
2016-09-30 15:32:41 -0500

    firebase database abstract model

--- expocity/Firebase/Database/Base/FDatabaseModel.swift
+++ expocity/Firebase/Database/Base/FDatabaseModel.swift
@@ -1,9 +1,9 @@
-//
-//  FDatabaseModel.swift
-//  expocity
-//
-//  Created by zero on 9/30/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class FDatabaseModel
+{
+    func modelJson() -> [String:AnyObject]
+    {
+        fatalError()
+    }
+}
