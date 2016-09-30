a4935e7b99bacf95e2ee9831c78d07dfacc60764
vauxhall <zero.griffin@gmail.com>
2016-09-30 15:58:01 -0500

    added status to user firebase

--- expocity/Firebase/Database/Models/FDatabaseModelUser.swift
+++ expocity/Firebase/Database/Models/FDatabaseModelUser.swift
@@ -4,24 +4,34 @@ class FdatabaseModelUser:FDatabaseModel
 {
     let name:String
     let created:TimeInterval
+    let status:Status
     
     enum Property:String
     {
         case name = "name"
         case created = "created"
+        case status = "status"
+    }
+    
+    enum Status:Int
+    {
+        case active = 0
+        case banned = 1
     }
     
     init(name:String)
     {
         self.name = name
         created = NSDate().timeIntervalSince1970
+        status = Status.active
     }
 
     override func modelJson() -> [String:Any]
     {
         let json:[String:Any] = [
             Property.name.rawValue:name,
-            Property.created.rawValue:created
+            Property.created.rawValue:created,
+            Property.status.rawValue:status
         ]
         
         return json
