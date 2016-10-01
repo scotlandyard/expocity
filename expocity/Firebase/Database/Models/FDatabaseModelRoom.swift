8c349b9162f35cafba9a2b3b851db78b5bf1884e
vauxhall <zero.griffin@gmail.com>
2016-10-01 13:07:44 -0500

    room model

--- expocity/Firebase/Database/Models/FDatabaseModelRoom.swift
+++ expocity/Firebase/Database/Models/FDatabaseModelRoom.swift
@@ -1,6 +1,30 @@
 import Foundation
 
-class FDatabaseModelRoom
+class FDatabaseModelRoom:FDatabaseModel
 {
+    var name:String
+    var access:Access
+    let created:TimeInterval
     
+    enum Property:String
+    {
+        case name = "name"
+        case created = "created"
+        case access = "access"
+    }
+    
+    enum Access:Int
+    {
+        case invitationOnly = 0
+        case freeAccess = 1
+    }
+    
+    init(name:String, access:Access)
+    {
+        self.name = name
+        self.access = access
+        created = NSDate().timeIntervalSince1970
+        
+        super.init()
+    }
 }
