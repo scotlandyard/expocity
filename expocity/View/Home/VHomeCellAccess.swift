e12233e1e5242d9ad7e0a2f20e141e38178e8fea
vauxhall <zero.griffin@gmail.com>
2016-10-03 15:44:11 -0500

    cell access labels

--- expocity/View/Home/VHomeCellAccess.swift
+++ expocity/View/Home/VHomeCellAccess.swift
@@ -2,33 +2,59 @@ import UIKit
 
 class VHomeCellAccess:VHomeCell
 {
+    weak var labelInvitationOnly:UILabel!
+    weak var labelFreeJoin:UILabel!
+    weak var buttonInvitationOnly:UIButton!
+    weak var buttonFreeJoin:UIButton!
+    weak var model:MHomeItemAccess!
+    private let kTitleHeight:CGFloat = 20
+    private let kLabelsWidth:CGFloat = 100
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         
-        let base:UIView = UIView()
-        base.isUserInteractionEnabled = false
-        base.translatesAutoresizingMaskIntoConstraints = false
-        base.backgroundColor = UIColor.complement()
-        base.layer.cornerRadius = 4
+        let labelTitle:UILabel = UILabel()
+        labelTitle.isUserInteractionEnabled = false
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.textAlignment = NSTextAlignment.center
+        labelTitle.font = UIFont.bold(size:14)
+        labelTitle.textColor = UIColor.complement()
+        labelTitle.text = NSLocalizedString("VHomeCellAccess_labelTitle", comment:"")
         
-        let label:UILabel = UILabel()
-        label.isUserInteractionEnabled = false
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.backgroundColor = UIColor.clear
-        label.textAlignment = NSTextAlignment.center
-        label.font = UIFont.bold(size:16)
-        label.textColor = UIColor.white
-        label.text = NSLocalizedString("VHomeCellCreate_label", comment:"")
+        let labelInvitationOnly:UILabel = UILabel()
+        labelInvitationOnly.isUserInteractionEnabled = false
+        labelInvitationOnly.translatesAutoresizingMaskIntoConstraints = false
+        labelInvitationOnly.backgroundColor = UIColor.clear
+        labelInvitationOnly.textAlignment = NSTextAlignment.right
+        labelInvitationOnly.font = UIFont.regular(size:13)
+        labelInvitationOnly.textColor = UIColor.main()
+        labelInvitationOnly.text = NSLocalizedString("VHomeCellAccess_labelInvitationOnly", comment:"")
+        self.labelInvitationOnly = labelInvitationOnly
         
-        addSubview(base)
-        addSubview(label)
+        let labelFreeJoin:UILabel = UILabel()
+        labelFreeJoin.isUserInteractionEnabled = false
+        labelFreeJoin.translatesAutoresizingMaskIntoConstraints = false
+        labelFreeJoin.backgroundColor = UIColor.clear
+        labelFreeJoin.textAlignment = NSTextAlignment.left
+        labelFreeJoin.font = UIFont.regular(size:13)
+        labelFreeJoin.textColor = UIColor.main()
+        labelFreeJoin.text = NSLocalizedString("VHomeCellAccess_labelFreeJoin", comment:"")
+        self.labelFreeJoin = labelFreeJoin
+        
+        addSubview(labelTitle)
+        addSubview(labelInvitationOnly)
+        addSubview(labelFreeJoin)
         
         let views:[String:UIView] = [
-            "label":label,
-            "base":base]
+            "labelTitle":labelTitle,
+            "labelInvitationOnly":labelInvitationOnly,
+            "labelFreeJoin":labelFreeJoin]
         
-        let metrics:[String:CGFloat] = [:]
+        let metrics:[String:CGFloat] = [
+            "titleHeight":kTitleHeight,
+            "labelsWidth":kLabelsWidth]
         
         addConstraints(NSLayoutConstraint.constraints(
             withVisualFormat:"H:|-0-[label]-0-|",
@@ -36,17 +62,17 @@ class VHomeCellAccess:VHomeCell
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:|-0-[label]-0-|",
+            withVisualFormat:"V:|-10-[label(titleHeight)]",
             options:[],
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"H:|-10-[base]-10-|",
+            withVisualFormat:"H:|-0-[labelInvitationOnly(labelsWidth)]",
             options:[],
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:|-5-[base]-5-|",
+            withVisualFormat:"H:[labelFreeJoin(labelsWidth)]-0-|",
             options:[],
             metrics:metrics,
             views:views))
