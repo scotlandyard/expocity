5dce0550ba30470613b35f37aa6688cda88ca35a
vauxhall <zero.griffin@gmail.com>
2016-10-04 11:43:49 -0500

    added constant snapshot to test firebase user

--- expocity/Test/Firebase/Database/Models/TFDatabaseModelUser.swift
+++ expocity/Test/Firebase/Database/Models/TFDatabaseModelUser.swift
@@ -3,11 +3,21 @@ import XCTest
 
 class TFDatabaseModelUser:XCTestCase
 {
+    private let kSnapshot:[String:Any] = [
+        "created":1475541121.581116,
+        "name":"John Test",
+        "rooms":[
+            "12345",
+            "67890",
+            "34567"
+        ]
+    ]
+    
     //MARK: -
     
     func testModelJson()
     {
-        let name:String = "John Test"
+        let name:String = kSnapshot["name"] as! String
         let nameKey:String = FDatabaseModelUser.Property.name.rawValue
         let createdKey:String = FDatabaseModelUser.Property.created.rawValue
         let roomsKey:String = FDatabaseModelUser.Property.rooms.rawValue
