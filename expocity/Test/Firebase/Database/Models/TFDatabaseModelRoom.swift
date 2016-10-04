353227713a74e3f4268840b3abda12b51fb4b375
vauxhall <zero.griffin@gmail.com>
2016-10-03 20:41:22 -0500

    test model json

--- expocity/Test/Firebase/Database/Models/TFDatabaseModelRoom.swift
+++ expocity/Test/Firebase/Database/Models/TFDatabaseModelRoom.swift
@@ -3,15 +3,33 @@ import XCTest
 
 class TFDatabaseModelRoom:XCTestCase
 {
-    private var firebaseRoom:FDatabaseModelRoom?
+    //MARK: -
     
-    override func setUp()
+    func testModelJson()
     {
-        super.setUp()
-    }
-    
-    override func tearDown()
-    {
-        super.tearDown()
+        let name:String = "MyRoom"
+        let access:FDatabaseModelRoom.Access = FDatabaseModelRoom.Access.freeJoin
+        let owner:String = "123456"
+        let nameKey:String = FDatabaseModelRoom.Property.name.rawValue
+        let accessKey:String = FDatabaseModelRoom.Property.access.rawValue
+        let ownerKey:String = FDatabaseModelRoom.Property.owner.rawValue
+        
+        let firebaseRoom:FDatabaseModelRoom = FDatabaseModelRoom(
+            name:name,
+            access:access,
+            owner:owner)
+        
+        let json:[String:Any] = firebaseRoom.modelJson()
+        let roomName:String? = json[nameKey] as? String
+        let roomOwner:String? = json[ownerKey] as? String
+        let roomRawAccess:Int? = json[accessKey] as? Int
+        
+        XCTAssertNotNil(roomRawAccess, "Invalid raw access")
+        
+        let rawAccess:FDatabaseModelRoom.Access? = FDatabaseModelRoom.Access(rawValue:roomRawAccess!)
+        
+        XCTAssertTrue(access == rawAccess, "Invalid access")
+        XCTAssertTrue(name == roomName, "Invalid name")
+        XCTAssertTrue(owner == roomOwner, "Invalid owner")
     }
 }
