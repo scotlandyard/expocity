257c415299f0b52bc60516545a3f40d08d3f6ae5
vauxhall <zero.griffin@gmail.com>
2016-10-04 13:08:00 -0500

    init model chat with roomId from firebase

--- expocity/Model/Chat/Main/MChat.swift
+++ expocity/Model/Chat/Main/MChat.swift
@@ -2,14 +2,15 @@ import Foundation
 
 class MChat
 {
+    let roomId:String
     var title:String
     var items:[MChatItem]
     var displayOption:MChatDisplayOptionsItem
     let annotations:MChatDisplayAnnotations
     
-    init(title:String)
+    init(roomId:String)
     {
-        self.title = title
+        self.roomId = roomId
         items = []
         displayOption = MChatDisplayOptionsItemFill()
         annotations = MChatDisplayAnnotations()
