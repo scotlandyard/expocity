a223116196ab0eb336dfea8c9e35a90a98414f08
vauxhall <zero.griffin@gmail.com>
2016-10-04 15:00:28 -0500

    test firebase room

--- expocity/Test/Model/Chat/Main/TMChat.swift
+++ expocity/Test/Model/Chat/Main/TMChat.swift
@@ -5,7 +5,7 @@ class TMChat:XCTestCase
 {
     private let kRoomSnapshot:[String:Any] = [
         "presentation":1,
-        "owner":"John Test",
+        "owner":"123456",
         "name":"My fancy room",
         "created":1475541121.581116,
         "access":1
@@ -15,9 +15,29 @@ class TMChat:XCTestCase
     
     func testFirebaseRoom()
     {
-        /*let firebaseRoom:FDatabaseModelRoom = FDatabaseModelRoom(snapshot:[:])
-        let userId:String = firebaseRoom.owner
+        let snapshotRawPresentation:Int = kRoomSnapshot["presentation"] as! Int
+        let snapshotOwner:String = kRoomSnapshot["owner"] as! String
+        let snapshotName:String = kRoomSnapshot["name"] as! String
+        let snapshotCreated:TimeInterval = kRoomSnapshot["created"] as! TimeInterval
+        let snapshotRawAccess:Int = kRoomSnapshot["access"] as! Int
+        let snapshotPresentation:FDatabaseModelRoom.Presentation = FDatabaseModelRoom.Presentation(
+            rawValue:snapshotRawPresentation)!
+        let snapshotAccess:FDatabaseModelRoom.Access = FDatabaseModelRoom.Access(
+            rawValue:snapshotRawAccess)!
         
-        XCTAssertFalse(userId.isEmpty, "Room has no owner")*/
+        let firebaseRoom:FDatabaseModelRoom = FDatabaseModelRoom(snapshot:kRoomSnapshot)
+        let chat:MChat = MChat(firebaseRoom:firebaseRoom)
+        
+        let chatPresentation:FDatabaseModelRoom.Presentation = chat.presentation
+        let chatOwner:String = chat.owner
+        let chatName:String = chat.title
+        let chatCreated:TimeInterval = chat.created
+        let chatAccess:FDatabaseModelRoom.Access = chat.access
+        
+        XCTAssertEqual(snapshotPresentation, chatPresentation, "Invalid presentation")
+        XCTAssertEqual(snapshotOwner, chatOwner, "Invalid owner")
+        XCTAssertEqual(snapshotName, chatName, "Invalid title")
+        XCTAssertEqual(snapshotCreated, chatCreated, "Invalid created timestamp")
+        XCTAssertEqual(snapshotAccess, chatAccess, "Invalid access")
     }
 }
