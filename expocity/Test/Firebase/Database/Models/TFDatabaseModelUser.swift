a8a54aabfffc78485d67b7b67df8866aaadafe69
vauxhall <zero.griffin@gmail.com>
2016-10-04 11:34:07 -0500

    test model json for firebase user

--- expocity/Test/Firebase/Database/Models/TFDatabaseModelUser.swift
+++ expocity/Test/Firebase/Database/Models/TFDatabaseModelUser.swift
@@ -8,10 +8,21 @@ class TFDatabaseModelUser:XCTestCase
     func testModelJson()
     {
         let name:String = "John Test"
+        let nameKey:String = FDatabaseModelUser.Property.name.rawValue
+        let createdKey:String = FDatabaseModelUser.Property.created.rawValue
+        let roomsKey:String = FDatabaseModelUser.Property.rooms.rawValue
         
         let firebaseUser:FDatabaseModelUser = FDatabaseModelUser(
             name:name)
         
         let json:[String:Any] = firebaseUser.modelJson()
+        let userName:String? = json[nameKey] as? String
+        let userCreated:Int? = json[createdKey] as? Int
+        let userRooms:[String]? = json[roomsKey] as? [String]
+        
+        XCTAssertTrue(name == userName, "Invalid name")
+        XCTAssertNotNil(userCreated, "Not storing user creation timestamp")
+        XCTAssertGreaterThan(userCreated!, 1, "Invalid timestamp")
+        XCTAssertNotNil(userRooms, "Invalid user rooms")
     }
 }
