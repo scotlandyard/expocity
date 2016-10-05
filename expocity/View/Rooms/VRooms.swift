4e8f700b24a808cc2b0a374077a2680af09aabe0
vauxhall <zero.griffin@gmail.com>
2016-10-05 12:39:31 -0500

    collection view in rooms

--- expocity/View/Rooms/VRooms.swift
+++ expocity/View/Rooms/VRooms.swift
@@ -5,6 +5,8 @@ class VRooms:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICol
     weak var controller:CRooms!
     weak var spinner:VMainLoader?
     weak var collectionView:UICollectionView!
+    private let kCollectionFooter:CGFloat = 80
+    private let kLineSpacing:CGFloat = 1
     
     convenience init(controller:CRooms)
     {
@@ -14,10 +16,51 @@ class VRooms:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICol
         clipsToBounds = true
         self.controller = controller
         
+        let barHeight:CGFloat = controller.parentController.viewParent.kBarHeight
+        
         let spinner:VMainLoader = VMainLoader()
         self.spinner = spinner
         
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.headerReferenceSize = CGSize.zero
+        flow.footerReferenceSize = CGSize(width:0, height:kCollectionFooter)
+        flow.minimumLineSpacing = kLineSpacing
+        flow.minimumInteritemSpacing = 0
+        flow.scrollDirection = UICollectionViewScrollDirection.vertical
+        
+        let collectionView:UICollectionView = UICollectionView(frame:CGRect.zero, collectionViewLayout:flow)
+        self.collectionView = collectionView
+        
+        addSubview(collectionView)
         addSubview(spinner)
+        
+        let views:[String:UIView] = [
+            "collectionView":collectionView,
+            "spinner":spinner]
+        
+        let metrics:[String:CGFloat] = [
+            "barHeight":barHeight]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[loader]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-(barHeight)-[loader]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[spinner]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-(barHeight)-[spinner]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
     
     //MARK: public
@@ -28,5 +71,6 @@ class VRooms:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICol
     }
     
     //MARK: collection delegate
+    
     
 }
