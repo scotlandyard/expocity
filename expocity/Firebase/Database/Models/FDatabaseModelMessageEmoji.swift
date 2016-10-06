d353819ad7c9651c11b570c6371f8a6e182f0578
vauxhall <zero.griffin@gmail.com>
2016-10-06 17:18:03 -0500

    factory text and emoji

---
+++ expocity/Firebase/Database/Models/FDatabaseModelMessageEmoji.swift
@@ -0,0 +1,9 @@
+//
+//  FDatabaseModelMessageEmoji.swift
+//  expocity
+//
+//  Created by zero on 10/6/16.
+//  Copyright © 2016 iturbide. All rights reserved.
+//
+
+import Foundation
---
+++ expocity/Firebase/Database/Models/FDatabaseModelMessageText.swift
@@ -0,0 +1,9 @@
+//
+//  FDatabaseModelMessageText.swift
+//  expocity
+//
+//  Created by zero on 10/6/16.
+//  Copyright © 2016 iturbide. All rights reserved.
+//
+
+import Foundation
