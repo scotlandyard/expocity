9e2a70e320a384016b98b3a6cf67d21cac0c6c73
vauxhall <zero.griffin@gmail.com>
2016-10-10 10:25:10 -0500

    derived data

---
+++ DerivedData/expocity/Build/Intermediates/expocity.build/Debug-iphoneos/expocity.build/DerivedSources/CoreDataGenerated/DExpocity/DExpocity+CoreDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  DExpocity+CoreDataModel.swift
+//  
+//
+//  Created by zero on 10/10/16.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
