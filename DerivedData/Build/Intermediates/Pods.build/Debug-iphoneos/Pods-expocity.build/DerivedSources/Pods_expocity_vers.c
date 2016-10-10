615e66e6b594a87209f78d48dc0a5e01e0eb9d98
vauxhall <zero.griffin@gmail.com>
2016-10-10 11:24:09 -0500

    changes

---
+++ DerivedData/Build/Intermediates/Pods.build/Debug-iphoneos/Pods-expocity.build/DerivedSources/Pods_expocity_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_expocityVersionString[];
+ extern const double Pods_expocityVersionNumber;
+
+ const unsigned char Pods_expocityVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_expocity  PROJECT:Pods-1" "\n";
+ const double Pods_expocityVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ DerivedData/Build/Intermediates/Pods.build/Debug-iphoneos/Pods-expocity.build/Objects-normal/arm64/Pods_expocity_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /devel/iturbide/expocity/DerivedData/Build/Intermediates/Pods.build/Debug-iphoneos/Pods-expocity.build/DerivedSources/Pods_expocity_vers.c
